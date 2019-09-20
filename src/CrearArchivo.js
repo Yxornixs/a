0265c73b0c974ba115145db0a6ec94d8c8c82506
Yxornixs <54190075+Yxornixs@users.noreply.github.com>
2019-09-20 17:04:22 -0300

    prueba
    prueba

--- src/CrearArchivo.js
+++ src/CrearArchivo.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
 
+fs.writeFile(
+  "./texto.txt",
+  "Hacemos la prueba de Arquitectura Web - Esta es mi primer prueba de la lectura de un archivo desde node.js para la materia",
+  error => {
+    if (error) console.log(error);
+    else console.log("El archivo texto.txt fue creado");
+  }
+);
+
 fs.writeFile("./archivo1.txt", "línea 1\nLínea 2", error => {
   if (error) console.log(error);
-  else console.log("El archivo fue creado");
+  else console.log("El archivo 1.txt fue creado");
 });
 
 console.log("última línea del programa");
